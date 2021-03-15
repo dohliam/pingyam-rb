b7bf1ffa6ff1e0e403808d3a5d94d188e9b40454
dohliam <dohliam@gmail.com>
2021-03-15 04:42:19 -0700

    +syllable modification

--- lib_pingyam.rb
+++ lib_pingyam.rb
@@ -1,18 +1,23 @@
 class Converter
   def initialize(base_rom=0)
     @dict = read_dict(base_rom)
-  end 
+  end
 
-  def convert_syllable(dict, word, method)
+  def convert_syllable(dict, word, method, mods=nil)
     w = dict[word.downcase]
     if w
-      method == 11 ? w : w[method]
+      syllable = w[method]
+      if mods
+        get_modifications(syllable, mods)
+      else
+        method == 11 ? w : syllable
+      end
     else
       word
     end
   end
 
-  def read_dict(base_rom)
+  def read_dict(base_rom=0)
     pwd = Dir.pwd
     Dir.chdir(File.dirname(__FILE__))
     file = File.open("pingyam/pingyambiu")
@@ -25,7 +30,7 @@ class Converter
     dict
   end
 
-  def convert_line(line, method)
+  def convert_line(line, method, mods=nil)
     line_array = line.split(/\s+/)
     result = ""
     if method == 11
@@ -37,7 +42,7 @@ class Converter
       end
     else
       line_array.each do |word|
-        result << convert_syllable(@dict, word, method) + " "
+        result << convert_syllable(@dict, word, method, mods) + " "
       end
     end
     result.gsub(/\s+\Z/, "")
@@ -46,5 +51,27 @@ class Converter
   def check_syllable(word)
     w = @dict[word.downcase]
     w ? true : false
+  end
+
+  def to_superscript(syllable)
+    num_hash = {
+      "1" => "¹", "2" => "²", "3" => "³",
+      "4" => "⁴", "5" => "⁵", "6" => "⁶",
+      "7" => "⁷", "8" => "⁸", "9" => "⁹"
+    }
+    syllable.gsub!(/([1-9])/) { |d| num_hash[d] }
+  end
+
+  def normalize_yale(syllable)
+    num_hash = {
+      "7" => "1", "8" => "3", "9" => "6"
+    }
+    syllable.gsub!(/([789])/) { |d| num_hash[d] }
+  end
+
+  def get_modifications(syllable, mods)
+    normalize_yale(syllable) if mods[:yale]
+    to_superscript(syllable) if mods[:superscript]
+    syllable
   end
 end
